3a362e2d0b46f88553a62b21ba8b974b307b52a1
labpatron <labpatron@lab000001>
2024-03-25 10:37:35 -0400

    push

---
+++ ValleyVisionSolution/Pages/DataClasses/Message.cs
@@ -0,0 +1,14 @@
+﻿namespace ValleyVisionSolution.Pages.DataClasses
+{
+    public class Message
+    {
+        public int? MessageID { get; set; }
+
+        public string? MessageContent { get; set; }
+        public DateTime? MessageDateTime { get; set; }
+        public int? UserID { get; set; }
+        public int? InitID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
--- ValleyVisionSolution/Pages/DiscussionBoard/DiscussionBoardPage.cshtml.cs
+++ ValleyVisionSolution/Pages/DiscussionBoard/DiscussionBoardPage.cshtml.cs
@@ -1,12 +1,95 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using static BenchmarkDotNet.Engines.EngineEventSource;
+using ValleyVisionSolution.Pages.DataClasses;
+using System.Data.SqlClient;
+using ValleyVisionSolution.Pages.DB;
 
 namespace ValleyVisionSolution.Pages.DiscussionBoard
 {
     public class DiscussionBoardPageModel : PageModel
     {
-        public void OnGet()
+        public List<Message> Messages { get; set; }
+
+        public int initID { get; set; }
+
+        public int? InitiativeArea { get; set; }
+
+        public int? LoggedInUser { get; set; }
+
+        [BindProperty]
+        public Message NewMessage { get; set; }
+        
+        public DiscussionBoardPageModel()
         {
+            Messages = new List<Message>();
+            NewMessage = new Message();
+            NewMessage.MessageContent = "";
+        }
+
+
+
+            public void OnGet()
+        {
+            LoggedInUser = HttpContext.Session.GetInt32("UserID");
+            InitiativeArea = HttpContext.Session.GetInt32("InitID");
+            initID = (int)InitiativeArea;
+
+            SqlDataReader reader5 = DBClass.MessagesReader(initID);
+            while (reader5.Read())
+            {
+                Messages.Add(new Message
+                {
+                    MessageID = int.Parse(reader5["messageID"].ToString()),
+                    MessageContent = reader5["messageContent"].ToString(),
+                    MessageDateTime = Convert.ToDateTime(reader5["messageDateTime"]),
+                    UserID = int.Parse(reader5["userID"].ToString()),
+                    InitID = int.Parse(reader5["initID"].ToString()),
+                    FirstName = reader5["firstName"].ToString(),
+                    LastName = reader5["lastName"].ToString()
+                });
+            }
+            // Close your connection in DBClass
+            DBClass.ValleyVisionConnection.Close();
+        }
+       
+        public IActionResult OnPostSendMessage()
+        {
+            if (NewMessage.MessageContent != null)
+            {
+                NewMessage.UserID = HttpContext.Session.GetInt32("UserID");
+                NewMessage.InitID = HttpContext.Session.GetInt32("InitID");
+                NewMessage.MessageDateTime = DateTime.Now;
+                DBClass.AddMessage(NewMessage);
+                
+            }
+            
+            ModelState.Clear();
+            Messages = new List<Message>();
+            NewMessage = new Message();
+
+            LoggedInUser = HttpContext.Session.GetInt32("UserID");
+            InitiativeArea = HttpContext.Session.GetInt32("InitID");
+            initID = (int)InitiativeArea;
+
+            SqlDataReader reader5 = DBClass.MessagesReader(initID);
+            while (reader5.Read())
+            {
+                Messages.Add(new Message
+                {
+                    MessageID = int.Parse(reader5["messageID"].ToString()),
+                    MessageContent = reader5["messageContent"].ToString(),
+                    MessageDateTime = Convert.ToDateTime(reader5["messageDateTime"]),
+                    UserID = int.Parse(reader5["userID"].ToString()),
+                    InitID = int.Parse(reader5["initID"].ToString()),
+                    FirstName = reader5["firstName"].ToString(),
+                    LastName = reader5["lastName"].ToString()
+                });
+            }
+            // Close your connection in DBClass
+            DBClass.ValleyVisionConnection.Close();
+
+            return Page();
         }
 
         public IActionResult OnPostLogoutHandler()
