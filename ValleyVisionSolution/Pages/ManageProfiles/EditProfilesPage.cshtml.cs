eb0eb2d6083bbaa0f1ce8ae81feff2673b048257
Shamlojx <159274714+Shamlojx@users.noreply.github.com>
2024-03-27 18:01:36 -0400

    push

--- ValleyVisionSolution/Pages/ManageProfiles/EditProfilesPage.cshtml.cs
+++ ValleyVisionSolution/Pages/ManageProfiles/EditProfilesPage.cshtml.cs
@@ -10,19 +10,58 @@ namespace ValleyVisionSolution.Pages.ManageProfiles
 {
     public class EditProfilesPageModel : PageModel
     {
+        public List<FullProfile> AllProfiles { get; set; }
+        public int? ViewedProfile { get; set; }
 
         [BindProperty]
-        public FullProfile EditedProfile { get; set; }
-        public int? ViewedProfile { get; set; }
-        public List<FullProfile> AllProfiles { get; set; } 
+        public FullProfile ProfileToUpdate { get; set; }
 
         public EditProfilesPageModel()
         {
+            ProfileToUpdate = new FullProfile();
             AllProfiles = new List<FullProfile>();
         }
 
-        public void OnGet()
+        public void OnGet (int userid)
         {
+            HttpContext.Session.SetInt32("UserID", userid);
+            ViewedProfile = HttpContext.Session.GetInt32("UserID"); 
+
+
+            SqlDataReader singleUser= DBClass.SingleProfilesReader(userid);
+                while (singleUser.Read())
+                {
+                    ProfileToUpdate.UserID = userid;
+                    ProfileToUpdate.UserName = singleUser["UserName"].ToString();
+                    ProfileToUpdate.Password = singleUser["Password"].ToString();
+                    ProfileToUpdate.FirstName = singleUser["FirstName"].ToString();
+                    ProfileToUpdate.LastName = singleUser["LastName"].ToString();
+                    ProfileToUpdate.Email = singleUser["Email"].ToString();
+                    ProfileToUpdate.Phone = singleUser["Phone"].ToString();
+                    ProfileToUpdate.Street = singleUser["Street"].ToString();
+                    ProfileToUpdate.Apartment = singleUser["Apartment"].ToString();
+                    ProfileToUpdate.City = singleUser["City"].ToString();
+                    ProfileToUpdate.State = singleUser["State"].ToString();
+                    ProfileToUpdate.Zip = Int32.Parse(singleUser["Zip"].ToString());
+                    ProfileToUpdate.Country = singleUser["UserName"].ToString();
+                }
+                DBClass.ValleyVisionConnection.Close();
+
+            //foreach (var profile in AllProfiles)
+            //{
+            //    if (profile.UserID == ViewedProfile)
+            //    {
+            //        UserName = profile.UserName;
+            //    }
+            //}
+        }
+
+       
+        public IActionResult OnPost()
+        {
+            DBClass.UpdateProfile(ProfileToUpdate);
+            DBClass.ValleyVisionConnection.Close();
+            return RedirectToPage("Index");
         }
     }
 }
