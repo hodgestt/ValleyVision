a5b4ecbcf81d175d504d42eca94c805dd0d65a6e
nickpatt12 <patterns@dukes.jmu.edu>
2024-03-27 18:35:51 -0400

    initiative icons

--- ValleyVisionSolution/Pages/DataClasses/Initiative.cs
+++ ValleyVisionSolution/Pages/DataClasses/Initiative.cs
@@ -7,6 +7,7 @@ namespace ValleyVisionSolution.Pages.DataClasses
         public int? InitID { get; set; }
         [Required]
         public string? InitName { get; set; }
+        public string? FilePath { get; set; }
         public DateTime? InitDateTime { get; set; }
     }
 }
