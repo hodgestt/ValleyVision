f557c45d8e2501ab802af06266c7d5bf425827b1
TomasCastroAlbano <tcastroalbano@gmail.com>
2024-03-28 12:41:31 -0400

    p

---
+++ ValleyVisionSolution/Pages/DataClasses/Expenditure.cs
@@ -0,0 +1,15 @@
+﻿namespace ValleyVisionSolution.Pages.DataClasses
+{
+    public class Expenditure
+    {
+        public int Year { get; set; }
+        public decimal InflationRate { get; set; }
+        public decimal InterestRate { get; set; }
+        public decimal PublicSafety { get; set; }
+        public decimal School { get; set; }
+        public decimal Anomoly { get; set; }
+        public decimal Other { get; set; }
+        public decimal TotalExpenditure { get; set; }
+
+    }
+}
--- ValleyVisionSolution/Pages/HistoricalSpending/HistoricalSpendingPage.cshtml.cs
+++ ValleyVisionSolution/Pages/HistoricalSpending/HistoricalSpendingPage.cshtml.cs
@@ -1,12 +1,53 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using ValleyVisionSolution.Pages.DataClasses;
+using ValleyVisionSolution.Pages.DB;
 
 namespace ValleyVisionSolution.Pages.HistoricalSpending
 {
     public class HistoricalSpendingPageModel : PageModel
     {
-        public void OnGet()
+        public List<Expenditure> HistoricalExpenditures { get; set; }
+
+        public HistoricalSpendingPageModel() 
         {
+
+        }
+
+        public void loadData()
+        {
+            //Populate current year revenue data
+            SqlDataReader reader = DBClass.HistoricalExpendituresReader();
+            while (reader.Read())
+            {
+                HistoricalExpenditures.Add(new Expenditure
+                {
+                    Year = int.Parse(reader["Year_"].ToString()),
+                    InflationRate = Decimal.Parse(reader["InflationRate"].ToString()),
+                    InterestRate = Decimal.Parse(reader["InterestRate"].ToString()),
+                    PublicSafety = Decimal.Parse(reader["PublicSafety"].ToString()),
+                    School = Decimal.Parse(reader["School"].ToString()),
+                    Anomoly = Decimal.Parse(reader["Anomoly"].ToString()),
+                    Other = Decimal.Parse(reader["Other"].ToString()),
+                    TotalExpenditure = Decimal.Parse(reader["TotalExpenditure"].ToString())
+                });
+            }
+            // Close your connection in DBClass
+            DBClass.ValleyVisionConnection.Close();
+        }
+
+        public IActionResult OnGet()
+        {
+            if (HttpContext.Session.GetString("LoggedIn") == "True")
+            {
+                loadData();
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Index");
+            }
         }
 
 
