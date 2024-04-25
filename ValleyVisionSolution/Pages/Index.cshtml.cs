eb4e61ac45e4837340957d25474f2b2fbb225356
Fitzsicw <150729058+FitzCodess@users.noreply.github.com>
2024-04-25 19:30:17 -0400

    ddadad

--- ValleyVisionSolution/Pages/Index.cshtml.cs
+++ ValleyVisionSolution/Pages/Index.cshtml.cs
@@ -31,7 +31,8 @@ namespace ValleyVisionSolution.Pages
                 };
 
                 // If a search term is provided, only add files that match the term.
-                if (string.IsNullOrEmpty(searchTerm) || file.FileName_.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                if (string.IsNullOrEmpty(searchTerm) || file.FileName_.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) || 
+                    file.publishdate.ToString().Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
                 {
                     PublishedResources.Add(file);
                 }
@@ -43,31 +44,7 @@ namespace ValleyVisionSolution.Pages
             PublishedResources = PublishedResources.OrderByDescending(file => file.UploadedDateTime).ToList();
         }
 
-        //public void OnGet(string searchTerm)
-        //{
-        //    HttpContext.Session.Remove("InitName");
-        //    SqlDataReader reader = DBClass.PublishReader();
-        //    while (reader.Read())
-        //    {
-        //        var file = new FileMeta
-        //        {
-        //            FileMetaID = int.Parse(reader["FileMetaID"].ToString()),
-        //            FileName_ = reader["FileName_"].ToString(),
-        //            FilePath = reader["FilePath"].ToString(),
-        //            FileType = reader["FileType"].ToString(),
-        //            UploadedDateTime = DateTime.Parse(reader["UploadedDateTime"].ToString()),
-        //            userID = int.Parse(reader["userID"].ToString()),
-        //        };
 
-        //        // If a search term is provided, only add files that match the term.
-        //        if (string.IsNullOrEmpty(searchTerm) || file.FileName_.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
-        //        {
-        //            PublishedResources.Add(file);
-        //        }
-        //    }
-        //    reader.Close();
-        //    DBClass.ValleyVisionConnection.Close();
-        //}
         public IActionResult OnPostLogoutHandler()
         {
             HttpContext.Session.Clear();
@@ -79,43 +56,4 @@ namespace ValleyVisionSolution.Pages
 
 
 
-//using ValleyVisionSolution.Pages.DataClasses;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-//using System.ComponentModel.DataAnnotations;
-//using ValleyVisionSolution.Pages.DB;
-//using System.Data.SqlClient;
 
-//namespace ValleyVisionSolution.Pages
-//{
-//    public class IndexModel : PageModel
-//    {
-//        public List<FileMeta> PublishedResources { get; set; } = new List<FileMeta>();
-
-//        public void OnGet()
-//        {
-//            HttpContext.Session.Remove("InitName");
-//            SqlDataReader reader = DBClass.PublishReader();
-//            while (reader.Read())
-//            {
-//                PublishedResources.Add(new FileMeta
-//                {
-//                    FileMetaID = int.Parse(reader["FileMetaID"].ToString()),
-//                    FileName_ = reader["FileName_"].ToString(),
-//                    FilePath = reader["FilePath"].ToString(),
-//                    FileType = reader["FileType"].ToString(),
-//                    UploadedDateTime = DateTime.Parse(reader["UploadedDateTime"].ToString()),
-//                    userID = int.Parse(reader["userID"].ToString()),
-//                });
-//            }
-//            reader.Close();
-//            DBClass.ValleyVisionConnection.Close();
-//        }
-
-//        public IActionResult OnPostLogoutHandler()
-//        {
-//            HttpContext.Session.Clear();
-//            return RedirectToPage("/Index");
-//        }
-//    }
-//}
